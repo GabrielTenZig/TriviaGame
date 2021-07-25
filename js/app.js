bc1148b3edb4fa1401d4140146fac69f765dc95f
GabrielTenZig <jesusgabrieltenorioziga@gmail.com>
2021-07-25 02:04:42 -0500

    Seccion de preguntas funcionando, aunque de forma infinita
    - La dinamica está funcionando
    - Por ahora solo hay 3 preguntas y se muestran de forma aleatoria.
    - Se muestran comentarios personalizados despues de verificar
        la respuesta.
    - La dinamca se torna infinita por el momento, no hay un metodo de
        paró, pero se desarrollara mas adelante.

--- js/app.js
+++ js/app.js
@@ -21,11 +21,12 @@ const puntaje = document.getElementById('puntaje')
 
 play_buton.addEventListener('click', () => {
     console.log("Iniciando juego")
+
     menu.style.zIndex = 0
     preguntas.style.zIndex = 1
     puntaje.style.zIndex = 0
 
-    state.pregunta = obtenerUnaPregunta()
+    renderizar_nueva_pregunta()
 
     console.group("Estado")
     console.log(state)
@@ -36,15 +37,71 @@ play_buton.addEventListener('click', () => {
 /**************************************
  *      SECCIÓN PREGUNTAS
 **************************************/
+
+
+/******** RENDERIZAR PREGUNTA ***********/
+let texto_pregunta = document.getElementById('texto_pregunta')
+
+let respuesta1 = document.getElementById('respuesta1')
+let respuesta1_label = document.getElementById('respuesta1_label')
+
+let respuesta2 = document.getElementById('respuesta2')
+let respuesta2_label = document.getElementById('respuesta2_label')
+
+let respuesta3 = document.getElementById('respuesta3')
+let respuesta3_label = document.getElementById('respuesta3_label')
+
+let respuesta4 = document.getElementById('respuesta4')
+let respuesta4_label = document.getElementById('respuesta4_label')
+
+
+const renderizar_nueva_pregunta = () => {
+    state.pregunta = obtenerUnaPregunta()
+
+    respuesta1.checked = respuesta2.checked = respuesta3.checked = respuesta4.checked = false
+
+    texto_pregunta.textContent = state.pregunta.pregunta
+    
+    respuesta1_label.textContent = respuesta1.value = state.pregunta.respuestas[0]
+    respuesta2_label.textContent = respuesta2.value = state.pregunta.respuestas[1]
+    respuesta3_label.textContent = respuesta3.value = state.pregunta.respuestas[2]
+    respuesta4_label.textContent = respuesta4.value = state.pregunta.respuestas[3]
+}
+
+/****************************************** 
+ *          VERIFICAR RESPUESTA
+******************************************/
 const verificar_respuesta = document.getElementById('verificar_respuesta')
 const siguiente_pregunta = document.getElementById('siguiente_pregunta')
+const comentario_del_personaje = document.getElementById('comentario_del_personaje')
 
 verificar_respuesta.addEventListener('click', () => {
     console.log("Verificando respuesta...")
     siguiente_pregunta.style.display = "block"
     verificar_respuesta.style.display = "none"
+
+    let respuestas = [respuesta1, respuesta2, respuesta3, respuesta4]
+
+    for (let i = 0; i < respuestas.length; i++) {
+        if (respuestas[i].checked) {
+            if (respuestas[i].value === state.pregunta.respuesta_correcta) {
+                console.log("11111111111111111111111 ACERTÓ")
+                console.log("CC::", state.pregunta.comentarios[i])
+            }
+            comentario_del_personaje.textContent = state.pregunta.comentarios[i]
+        }
+
+    }
 })
 
+
+/****************************************** 
+ *          SIGUIENTE PREGUNTA
+******************************************/
 siguiente_pregunta.addEventListener('click', () => {
     console.log("Siguiente pregunta...")
+    renderizar_nueva_pregunta()
+    siguiente_pregunta.style.display = "none"
+    verificar_respuesta.style.display = "block"
 })
+
--- js/preguntas.js
+++ js/preguntas.js
@@ -1,12 +1,17 @@
 const obtenerUnaPregunta = () => {
-    return setDePreguntas["1"]["sheldon"][0]
+    const min = 0
+    const max = otroSetDePreguntas.length
+    console.log("Max:",max)
+    let numRand = Math.floor( Math.random() * (max - min) + min )
+    console.log("Obteniendo la pregunta numero:", numRand)
+    return otroSetDePreguntas[numRand]
 }
 
 const setDePreguntas = {
     "1": {
         "sheldon": [
             {
-                pregunta: "¿Cual es el nombre de la novia de Sheldon?",
+                pregunta: "¿Cual es el nombre de la novia de Sheldon?...",
                 respuestas: [
                     "Jermaioni", 
                     "Natalia Laforcade", 
@@ -33,8 +38,56 @@ const setDePreguntas = {
     }
 }
 
-// const setDePreguntas = [
-    
-// ]
+const otroSetDePreguntas = [
+    {
+        pregunta: "¿Cual es el nombre de mi novia?",
+        respuestas: [
+            "Jermaioni", 
+            "Natalia Laforcade", 
+            "Eimy", 
+            "Lenard"
+        ],
+        respuesta_correcta: "Eimy",
+        comentarios: [
+            "¿Como dices?, ni siquiera la conosco",
+            "Ciertamente Natalia es una excelente artista, pero me parece que has fallado",
+            "Por supuesto, ¡acertaste!... mi dulce Eimy",
+            "¿Queeeé?, aprecio al sujeto, pero no... ya quisiera",
+        ]
+    },
+    {
+        pregunta: "¿Cuantos premios Novel ha ganado?",
+        respuestas: [
+            "1", 
+            "2", 
+            "5", 
+            "Ninguno"
+        ],
+        respuesta_correcta: "1",
+        comentarios: [
+            "Acertaste, y debo agregar que estoy muy orgulloso de mi premio, y quiza un poco de tu habilidad para recordarlo.",
+            "No, no he ganado dos premios Novel, aunque entiendo tu confucion.",
+            "Erraste, pero entiendo que mi inteligencia te confunda un poco.",
+            "¿Queeeé?, he de decir que eso me ofende muchisimo.",
+        ]
+    },
+    {
+        pregunta: "¿Como se llama el juego que solemos jusgar mis amigos y yo?",
+        respuestas: [
+            "Monopoly", 
+            "Calabosos y dragones", 
+            "Make and Breake", 
+            "Scrabble"
+        ],
+        respuesta_correcta: "Calabosos y dragones",
+        comentarios: [
+            "No, ciertamente nunca he jugado monopoly, pero un dia de estos quiza.",
+            "Por supuesto, ¿quierés jusgar una partida con nosotros?",
+            "No, no tengo tanta destreza con las manos, no ganaría",
+            "He llegado a jugarlo, pero no es de mis juegos frecuentes.",
+        ]
+    }
+
+]
 
 export { obtenerUnaPregunta }
